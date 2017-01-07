faba6a38f654d5d489de7c01e7a562f82bcc6c3f
Hongyu Wang <hongyu.wang@mail.utoronto.ca>
2017-01-07 15:09:26 -0500

    project start

---
+++ html/src/com/mygdx/boxhead/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.mygdx.boxhead.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.mygdx.boxhead.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MyGdxGame();
+        }
+}
