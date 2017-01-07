8e61067efc304af259b9e6ef4d3e65a04f39860a
Darwin Truong <darwin.truong@outlook.com>
2017-01-07 17:08:32 -0500

    setup material

--- webclient/src/app.jsx
+++ webclient/src/app.jsx
@@ -1,14 +1,11 @@
 import styles from './css/index.scss';
 import React from 'react';
+import Landing from './components/front.js';
 
 export default class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>It Works!</h1>
-        <p>This React project just works including <span className={styles.redBg}>module</span> local styles.</p>
-        <p>Enjoy!</p>
-      </div>
+    	<Landing />
     )
   }
 }
