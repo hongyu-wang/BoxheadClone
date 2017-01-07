b8bfc7083f659fa021130438ec3a596240c1e4f7
Darwin Truong <darwin.truong@outlook.com>
2017-01-07 17:09:50 -0500

    im a dumbass

---
+++ webclient/src/components/front.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+export default class Landing extends React.Component {
+	render() {
+		return (
+			<div>
+				<AppBar
+					title="Boxhead Clone"
+				/>
+			</div>
+		);
+	}
+};
