1526b16b1e5031900721196601f3c6ec26106b2a
Darwin Truong <darwin.truong@outlook.com>
2017-01-07 21:37:20 -0500

    playermodel

--- core/src/server/Model/PlayerModel.java
+++ core/src/server/Model/PlayerModel.java
@@ -4,6 +4,31 @@ package server.Model;
  * Created by hongy on 1/7/2017.
  */
 public class PlayerModel {
+    private int hp;
+    private int[] dir;
+    private int[] pos;
 
+    public int getHp() {
+        return hp;
+    }
 
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int[] getDir() {
+        return dir;
+    }
+
+    public void setDir(int[] dir) {
+        this.dir = dir;
+    }
+
+    public int[] getPos() {
+        return pos;
+    }
+
+    public void setPos(int[] pos) {
+        this.pos = pos;
+    }
 }
--- webclient/src/components/front.js
+++ webclient/src/components/front.js
@@ -3,10 +3,16 @@ import AppBar from 'material-ui/AppBar';
 
 export default class Landing extends React.Component {
 	render() {
+		let style = {
+			backgroundColor: "#D50000",
+		}
 		return (
 			<div>
 				<AppBar
 					title="Boxhead Clone"
+					style={style}
+					showMenuIconButton={false}
+					titleStyle={{fontSize: "50px", textAlign: "center", padding: "20px"}}
 				/>
 			</div>
 		);
