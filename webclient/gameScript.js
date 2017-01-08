1a1cd930cf22b54a967a7e006b078e816e9521e1
Marion Vasantharajah <marion.vasantharajah@mail.utoronto.ca>
2017-01-08 00:47:37 -0500

    Finished bullet movement

--- webclient/gameScript.js
+++ webclient/gameScript.js
@@ -1,6 +1,7 @@
 var map = [10000, 10000];
 var mapRange = 10000;
 var angle = 0;
+var bulletSize = 20
 var clientState = {
 	players : {
 		"player1ID": "blank",
@@ -10,14 +11,19 @@ var clientState = {
 	barriers : []
 }
 
-var bullet = {
+var bulletQ = {
 	position : [,],
 	angle : 0
 }
 
+function bulletObj(){
+	var position;
+	var angle;
+}
+
 function Player(ID)
 {
-    id  = ID
+    id = ID
 	position = [0, 0],
 	angle = 0	 
 }
@@ -31,7 +37,7 @@ function initGame(gameStateDict){
 		key: id,
 		value: PLAYER1
 	})
-	delete clientState.players["player1ID"];
+	delete clientState.players["player2ID"];
 	clientState.players.push({
 		key: id,
 		value: PLAYER2
@@ -58,7 +64,6 @@ function updateGame(gameStateDict){
 
 		if (gameStateDict[up] === "true"){
 			// increase player pos by 1
-			player1Pos[1]++;
 			angle = 90;
 		}
 		else if (gameStateDict[down] === "true"){
@@ -93,9 +98,84 @@ function updateGame(gameStateDict){
 	   }
 	}
 	if (gameStateDict[space] === "true"){
-			bullet.position = [0 , 0]
-			bullet.angle = angle
+			var newBullet = new bulletObj()
+			newBullet.position = clientState.players[id].position
+			newBullet.angle = angle
+			clientState.bulletsOnScreen.push(newBullet)
 		}
+
+	moveBullet()
+	// collisions
+	// move
+	return gameStateDict
+}
+
+function moveBullet(){
+	for (var i = 0; i < clientState.bulletsOnScreen.length; i++)
+	{
+		clientState.bulletsOnScreen[i].position = getBulletTravel(clientState.bulletsOnScreen[i]);
+	}
+}
+
+function getBulletTravel(bullet)
+{
+	var bulletPOS = bullet.position
+	if (bullet.angle === 90)
+	{
+		bulletPOS[1] += bulletSize;
+	}
+	else if (bullet.angle === 180)
+	{
+		bulletPOS[0] -= bulletSize;
+	}
+	else if (bullet.angle === 270)
+	{
+		bulletPOS[1] -= bulletSize;
+	}
+	else if (bullet.angle === 0)
+	{
+		bulletPOS[0] += bulletSize;
+	}
+	else if (bullet.angle === 45)
+	{
+		bulletPOS[0] += bulletSize;
+		bulletPOS[1] += bulletSize;
+	}
+	else if (bullet.angle === 135)
+	{
+		bulletPOS[0] -= bulletSize;
+		bulletPOS[1] += bulletSize
+	}
+	else if (bullet.angle === 225)
+	{
+		bulletPOS[0] -= bulletSize;
+		bulletPOS[1] -= bulletSize;
+	}
+	else if (bullet.angle === 180)
+	{
+		bulletPOS[0] += bulletSize;
+		bulletPOS[1] -= bulletSize;
+	}
+
+	return bulletPOS;
 }
 
 
+var PLAYER1 = new Player("gameStateDict[id]")
+var PLAYER2 = new Player("gameStateDict[id2]")
+var bullet1 = new bulletObj()
+bullet1.position = [4,3]
+bullet1.angle = 90
+console.log(bullet1.position)
+clientState = {
+	players : {
+		"gameStateDict[id]": PLAYER1,
+		"gameStateDict[id]": PLAYER2
+	},
+	bulletsOnScreen : [bullet1],
+	barriers : []
+}
+
+moveBullet()
+
+console.log(bullet1.position)
