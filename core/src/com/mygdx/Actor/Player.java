a177948739bcb942a679206f409029bff8246fa1
darknal43 <hongywang@hotmail.com>
2017-01-07 21:21:30 -0500

    Some more stick server

--- core/src/com/mygdx/Actor/Player.java
+++ core/src/com/mygdx/Actor/Player.java
@@ -1,9 +1,43 @@
 package com.mygdx.Actor;
 
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.Json;
+
+import java.util.ArrayList;
+
 /**
  *
  * Created by hongy on 1/7/2017.
  */
-public class Player {
+public class Player  {
+    private String name;
+    private int age;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main (String args []){
+        Player person = new Player();
+        person.setName("Nate");
+        person.setAge(31);
+        Json json = new Json();
+
+        String model = json.toJson(person);
+
+
+    }
 }
+
---
+++ core/src/server/Model/PlayerModel.java
@@ -0,0 +1,7 @@
+package server.Model;
+
+/**
+ * Created by hongy on 1/7/2017.
+ */
+public class PlayerModel {
+}
