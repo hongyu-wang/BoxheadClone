6e5fe77f8816411aca09e62b7f3e568ecb79db48
darknal43 <hongywang@hotmail.com>
2017-01-07 22:35:03 -0500

    Other code

--- core/src/com/mygdx/Actor/Player.java
+++ core/src/com/mygdx/Actor/Player.java
@@ -10,34 +10,6 @@ import java.util.ArrayList;
  * Created by hongy on 1/7/2017.
  */
 public class Player  {
-    private String name;
-    private int age;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public static void main (String args []){
-        Player person = new Player();
-        person.setName("Nate");
-        person.setAge(31);
-        Json json = new Json();
-
-        String model = json.toJson(person);
-
-
-    }
 }
 
