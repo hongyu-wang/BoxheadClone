ae94abc382fb7e266c15835b622df118a3e1fd5d
Darwin Truong <darwin.truong@outlook.com>
2017-01-16 11:27:01 -0500

    stuff

--- webclient/src/app.jsx
+++ webclient/src/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Landing from './components/front.js';
-import Test from './components/button.js';
 
 export default class App extends React.Component {
   render() {
